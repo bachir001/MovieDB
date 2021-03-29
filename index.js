f659630b1e79528905f75e08a1fff1b40b94c67a
bachir <mohamadbachir01@gmail.com>
2021-03-30 00:10:30 +0300

    step 3

--- index.js
+++ index.js
@@ -2,8 +2,28 @@ var express = require('express');
 var app = express();
 var PORT = 3000;
  
+const date = new Date();
+
+
+
+
 app.get('/', function (req, res) {
   res.send('ok');
+});
+ 
+
+app.get('/test', function (req, res) {
+  res.send({status:200, message:"ok"});
+});
+ 
+
+
+app.get('/time', function (req, res) {
+
+
+   
+  res.send({status:200,message: date.getHours()+":"+date.getSeconds() });
+
 });
  
 
