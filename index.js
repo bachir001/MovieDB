dc3aecbc2b504d35ec99f49bfeab2a246af72d01
bachir <mohamadbachir01@gmail.com>
2021-03-30 11:13:20 +0300

    step 5

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var app = express();
 var PORT = 3000;
- 
-const date = new Date();
+const date = new Date(); 
 
+const movies = [
+  { title: 'Jaws', year: 1975, rating: 8 },
+  { title: 'Avatar', year: 2009, rating: 7.8 },
+  { title: 'Brazil', year: 1985, rating: 8 },
+  { title: 'الإرهاب والكباب‎', year: 1992, rating: 6.2 }
+]
+
+
+// let Data = JSON.stringify(movies,null,2)
 
 
 
@@ -45,6 +53,32 @@ app.get('/search',function(req,res){
   }
 
 });
+
+
+
+
+
+
+app.get('/movies/create', function (req, res) {
+  res.send({status:200, message:"movies create "});
+});
+
+
+app.get('/movies/read', function (req, res) {
+  let data=movies;
+  res.send({status:200, message: data });
+
+});
+
+
+app.get('/movies/update', function (req, res) {
+  res.send({status:200, message:"movies update  "});
+});
+
+app.get('/movies/delete', function (req, res) {
+  res.send({status:200, message:"movies delete "});
+});
+
 
 
 // // Without middleware
