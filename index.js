46e5672c5b14d79814a334edd6b47493df02396d
bachir <mohamadbachir01@gmail.com>
2021-03-30 10:12:14 +0300

    step 4

--- index.js
+++ index.js
@@ -20,12 +20,47 @@ app.get('/test', function (req, res) {
 
 app.get('/time', function (req, res) {
 
-
-   
   res.send({status:200,message: date.getHours()+":"+date.getSeconds() });
 
 });
  
+
+
+
+
+app.get('/hello/:id', function (req, res) {
+  res.send({status:200, message:"Hello , "+req.params.id});
+});
+ 
+
+app.get('/search',function(req,res){
+
+  if(req.query.s){
+  res.send({status:200, message:"data : "+req.query.s});
+
+  
+  }else{
+
+    res.send( {status:500, error:true, message:"you have to provide a search"});
+  }
+
+});
+
+
+// // Without middleware
+// app.get('/', function(req, res){
+  
+//   // Equivalent to res.status(200).send('OK')
+//   res.sendStatus(200); 
+// });
+
+// app.listen(PORT, function(err){
+//   if (err) console.log(err);
+//   console.log("Server listening on PORT", PORT);
+// });
+
+
+
 
 app.listen(PORT);
 
